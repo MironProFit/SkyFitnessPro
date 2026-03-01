411f85dfbc0f1432e9884db3f064b38fdcb42d48
Your Name <you@example.com>
2026-03-01 13:28:08 +0500

    поет песни и пляски с атлетом на страницы описания

--- src/widgets/course-detail/CourseCta.tsx
+++ src/widgets/course-detail/CourseCta.tsx
@@ -1,38 +1,33 @@
 import styles from './CourseDetail.module.css';
-import CtaImage from '@/shared/assets/courses/cta_img.webp';
+import CtaImage from '@/shared/assets/courses/cta_img.png';
+import CtaImageLine from '@/shared/assets/courses/cta_line.png';
 
 interface CourseCtaProps {
   fitting?: string[];
 }
 
-
-
 export const CourseCta = ({ fitting }: CourseCtaProps) => {
-  const benefits = fitting && fitting.length > 0 ? fitting : [
-    'проработка всех групп мышц',
-    'тренировка суставов',
-    'улучшение циркуляции крови',
-  ];
+  const benefits =
+    fitting && fitting.length > 0
+      ? fitting
+      : ['проработка всех групп мышц', 'тренировка суставов', 'улучшение циркуляции крови'];
 
   return (
     <div className={styles.ctaBlock}>
       <div className={styles.ctaContent}>
         <h2 className={styles.ctaTitle}>Начните путь к новому телу</h2>
-        
+
         <ul className={styles.benefitsList}>
           {benefits.map((item, index) => (
             <li key={index}>{item.toLowerCase()}</li>
           ))}
         </ul>
 
-        <button className={styles.ctaButton}>
-          Войдите, чтобы добавить курс
-        </button>
+        <button className={styles.ctaButton}>Войдите, чтобы добавить курс</button>
       </div>
-      
-      <img src={CtaImage} className={styles.ctaImage}/>
+
+      <img src={CtaImageLine} className={styles.ctaImageLine} />
+      <img src={CtaImage} alt="Athlet" className={styles.ctaImage} />
     </div>
   );
 };
-
-     
